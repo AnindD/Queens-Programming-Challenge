c5df24dfc0c9f1bc977d232254be7c8a3e8a951f
Anindit Dewan <105120201+AnindD@users.noreply.github.com>
2022-07-31 22:38:34 -0400

    Update QueensCSProject.py

--- Queens_CS_Project_Folder/QueensCSProject.py
+++ Queens_CS_Project_Folder/QueensCSProject.py
@@ -3,7 +3,11 @@
 # Version: N/A 
 
 import pygame 
+import cv2 
+import numpy 
+import math 
 from pathlib import Path
+
 
 # initialize the necessary variables 
 pygame.init()
@@ -41,6 +45,11 @@ class Button():
             if pos[1] > self.y and pos[1] < self.y + self.height: 
                 return True 
         return False 
+    def check_mouse_screen(self, pos): 
+        if pos[0] > self.x and pos[0] < self.x + self.width and pos[1] > self.y and pos[1] < self.y + self.height: 
+            return True 
+        else: 
+            return False 
 
 # Points used in game. 
 class Number_Point(): 
@@ -60,16 +69,24 @@ class Number_Point():
                 return True 
         return False 
 
+class Circle_Number_Point(): 
+    def __init__(self, x, y): 
+        self.x = x
+        self.y = y 
+    def draw_point(self): 
+        pygame.draw.circle(win, (255,0,0), (self.x, self.y), 30, 3)
 # Images 
 PROJECT_ROOT = Path(__file__).parent.parent
 number_game_background = pygame.image.load(PROJECT_ROOT / "Queens_CS_Project_Folder/Number_Game_Background.png")
 number_game_background_2 = pygame.image.load(PROJECT_ROOT / "Queens_CS_Project_Folder/Number_Game_Background_2.png")
-brain = pygame.image.load(PROJECT_ROOT / "Queens_CS_Project_Folder/Brain.png")
+logo = pygame.image.load(PROJECT_ROOT / "Queens_CS_Project_Folder/Propel_logo.jpg")
+counter_starter_button = pygame.image.load(PROJECT_ROOT / "Queens_CS_Project_Folder/Counter_Starter_Button.png")
+
 
 
 # Fonts 
 number_game_font = pygame.font.Font(PROJECT_ROOT / "Queens_CS_Project_Folder/kremlin.ttf", 400)
-font = pygame.font.Font(PROJECT_ROOT / "Queens_CS_Project_Folder/ContrailOne-Regular.ttf", 60)
+font = pygame.font.Font(PROJECT_ROOT / "Queens_CS_Project_Folder/ContrailOne-Regular.ttf", 45)
 nav_font = pygame.font.SysFont(None, 40)
 
 # Buttons 
@@ -83,19 +100,41 @@ contact_us_button = Button((255, 0, 0), 970, 25, 180, 50, 2)
 exit_button = Button((255, 0, 0), 1250, 25, 100, 50, 2)
 nav_bar_buttons = [home_button, about_us_button, news_button, services_button, contact_us_button]
 
+game_one_okay_button = Button((255,0,0), 546, 538, 163, 62, 0)
+
 # All the screens as booleans, if one variable is true it will show that particular screen. It will automatically st art off with the start menu as true. 
 start_game = True
-game = False  
+game = False 
+counter_starter_button_boolean = True  
+start_first_game = False 
 
 # Extra Details 
 text = "a"
 counter = 0 
 
 
-# Thresholds 
+# Thresholds, for the first see line 252 
 changing_number_threshold = 1
 threshold_counter = 0 
 
+# Timer
+first_game_counter = 60
+first_timer_boolean = False 
+timer_event = pygame.USEREVENT+1
+pygame.time.set_timer(timer_event, 1000)
+
+# Arcs which will be used for the timer. 
+def drawArcCv2(surf, color, center, radius, width, end_angle):
+    circle_image = numpy.zeros((radius*2+4, radius*2+4, 4), dtype = numpy.uint8)
+    circle_image = cv2.ellipse(circle_image, (radius+2, radius+2),
+        (radius-width//2, radius-width//2), 0, 0, end_angle, (*color, 255), width, lineType=cv2.LINE_AA) 
+    circle_surface = pygame.image.frombuffer(circle_image.flatten(), (radius*2+4, radius*2+4), 'RGBA')
+    surf.blit(circle_surface, circle_surface.get_rect(center = center))
+
+def drawArc(surf, color, center, radius, width, end_angle):
+    arc_rect = pygame.Rect(0, 0, radius*2, radius*2)
+    arc_rect.center = center
+    pygame.draw.arc(surf, color, arc_rect, 0, end_angle, width)
 
 # List of all the points that will be clicked on. 
 number_point_list = [
@@ -109,6 +148,14 @@ number_point_list = [
     Number_Point(750, 528),  Number_Point(750, 528), Number_Point(831, 528), Number_Point(831, 528), Number_Point(750, 609), Number_Point(750, 609), Number_Point(832,608),  Number_Point(832,608),
     Number_Point(1029, 497), Number_Point(1029, 497), Number_Point(1109, 497), Number_Point(1109, 497), Number_Point(1029, 551), Number_Point(1029, 551), Number_Point(1109, 551), Number_Point(1109, 551), Number_Point(1030,606)
     ]
+
+# Circle points, for double clicks. 
+second_number_points = [
+    Circle_Number_Point(317, 509), Circle_Number_Point(240, 565), Circle_Number_Point(322, 620), 
+    Circle_Number_Point(565, 513), Circle_Number_Point(567, 577), Circle_Number_Point(566, 625), 
+    Circle_Number_Point(764, 540), Circle_Number_Point(845, 541), Circle_Number_Point(763, 626), Circle_Number_Point(848, 621), 
+    Circle_Number_Point(1044, 510), Circle_Number_Point(1124, 510), Circle_Number_Point(1043, 566), Circle_Number_Point(1124, 561), 
+]
 constant_len_list = len(number_point_list)
 
 number_list = [1,2,3,4,5,6,7,8,9,10]
@@ -127,7 +174,7 @@ def draw_start():
     quit_button.draw_button()
     for buttons in nav_bar_buttons: 
         buttons.draw_button() 
-    win.blit(brain, (769,50))
+    win.blit(logo, (850,250))
     win.blit(start_font, (250,170)) 
     win.blit(quit_font, (250, 470))
     win.blit(home_font, (110, 30))
@@ -135,31 +182,62 @@ def draw_start():
     win.blit(news_font, (550, 30))
     win.blit(services_font, (750, 30))
     win.blit(contact_us_font, (980, 30))
-
+    
 # Draw all necessary components for the mathematics game. 
 def draw_math_game(): 
     win.blit(number_game_background_2, (0,0))
     pygame.draw.rect(win, (0,0,0), (1505,0, 295, 900))
-    #clickable_text = number_game_font.render(str(changing_number_threshold), True, (255,0,0))
-    #win.blit(clickable_text, clickable_text.get_rect(center = win.get_rect().center))
+
+    for points in second_number_points: 
+        points.draw_point()
 
     instructions_font = font.render("Click on the red square !", True, (255,0,0))
+    game_1_okay_white = font.render("Okay", True, (0,0,0))
+    game_1_okay_black = font.render("Okay", True, (255,255,255))
     counter_font = font.render(f"Numbers clicked: {changing_number_threshold-1}/9, keep going!", True, (0,0,0))
+    secondary_counter_font = font.render(str(threshold_counter), True, (255,0,0))
     congrats_font = font.render("Congratulations, you've clicked on all numbers !", True, (0,0,0))
+    first_game_counter_text = font.render(str(first_game_counter), True, (0,122,0))
 
+    # Game text 
     if len(number_point_list) == constant_len_list:
         win.blit(instructions_font, (46,801))
     if len(number_point_list) > 0:
         win.blit(counter_font, (255,355))
+        win.blit(secondary_counter_font, (46, 801))
     elif len(number_point_list) == 0: 
         win.blit(congrats_font, (255,355))
     if len(number_point_list) > 0: 
         number_point_list[0].draw_point()
+    
+    # Okay button screen 
+    if counter_starter_button_boolean == True: 
+        win.blit(counter_starter_button, (200, 200))
+        game_one_okay_button.draw_button()
+        if game_one_okay_button.check_mouse_screen(mouse_position): 
+            game_one_okay_button.color = (255,255,255)
+            win.blit(game_1_okay_white, (580, 540))
+        else: 
+            game_one_okay_button.color = (0,0,0)
+            win.blit(game_1_okay_black, (580, 540))
+
+    # Timer 
+    pygame.draw.circle(win, (255,255,255), (1620,517), 77)
+    win.blit(first_game_counter_text, (1600, 495))
+    if first_game_counter != 0: 
+        drawArcCv2(win, (0, 122, 0), (1625, 520), 90, 20, 360*first_game_counter/60)
 
 while run: 
-    pygame.time.delay(50)
     for event in pygame.event.get(): 
         mouse_position = pygame.mouse.get_pos() 
+
+        if event.type == timer_event and first_timer_boolean == True:
+            if first_game_counter > 0: 
+                first_game_counter -= 1 
+            if first_game_counter == 0: 
+                first_game_counter = 0 
+                start_first_game = False 
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             if start_button.check_mouse_position(mouse_position) and start_game == True: 
                 fade()
@@ -169,36 +247,31 @@ while run:
             if quit_button.check_mouse_position(mouse_position) and start_game == True:
                 fade()
                 pygame.quit() 
+            if game == True and game_one_okay_button.check_mouse_position(mouse_position): 
+                counter_starter_button_boolean = False 
+                first_timer_boolean = True 
+                start_first_game = True 
             # Deletes points. 
             for number_points in number_point_list:
-                if number_points.check_mouse_position(mouse_position) and game == True: 
+                if number_points.check_mouse_position(mouse_position) and game == True and counter_starter_button_boolean == False and start_first_game == True: 
                     # Threshold counter will increase by 1 always when clicked 
                     threshold_counter += 1 
-                    if threshold_counter == changing_number_threshold: 
+                    if threshold_counter == changing_number_threshold:  # If capacity reached then a new threshold will be created, aka the next number (i.e. if user clicks on two then threshold will increase to three cause that is the next number)
                         changing_number_threshold += 1 
                         threshold_counter = 0 
-                    number_point_list.pop(number_point_list.index(number_points))
+                    number_point_list.pop(number_point_list.index(number_points)) 
                     number_points.color == (255,0,0)
-                    counter += 1 
-                    print("CLICKED ", counter)
-
-        if event.type == pygame.KEYDOWN and game == True:
-            if event.key == pygame.K_BACKSPACE:
-                text = text[:-1]
-            else: 
-                text += event.unicode
-                print("TYPED")
         print(mouse_position)  # Prints mouse position in console, useful for trying to place objects. 
         if event.type == pygame.QUIT:
             run = False
     if start_game == True:  
         draw_start()
     if game == True:
-        #print(threshold_counter)
-        #print(changing_number_threshold)
         draw_math_game()
+ 
         #for number_points in number_point_list:
             #number_points.draw_point()  
 
     pygame.display.update()
+
 pygame.quit()
