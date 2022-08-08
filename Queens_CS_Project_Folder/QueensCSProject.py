417bb36d4cd9096deed6599bf3e7a0e443626503
Anindit Dewan <105120201+AnindD@users.noreply.github.com>
2022-08-07 22:30:16 -0400

    Update QueensCSProject.py

--- Queens_CS_Project_Folder/QueensCSProject.py
+++ Queens_CS_Project_Folder/QueensCSProject.py
@@ -6,6 +6,8 @@ import cv2
 import numpy 
 import math 
 from pathlib import Path
+#import mysql.connector
+
 
 # initialize the necessary variables 
 pygame.init()
@@ -14,8 +16,21 @@ pygame.display.set_caption("Queens University Project") # Title
 clock = pygame.time.Clock() # The clock (will be useful later) 
 run = True 
 
-# This is just the initialized content so basically every time you open up pygame you need to set up a few things 
+""" Commented out because it could mess up downloads 
+# Database 
+database = mysql.connector.connect(
+    host="localhost", 
+    user="root", 
+    password="Ukusabmw123#",
+    database="testdatabase"
+)
 
+mycursor = database.cursor() 
+mycursor.execute("SELECT name FROM Users")
+
+for x in mycursor: 
+    print(x)
+"""
 
 # Fade animation, triggers after each button press. 
 def fade(): 
@@ -51,6 +66,22 @@ class Button():
             return True 
         else: 
             return False 
+class Text_Field(): 
+    def __init__(self, color, x, y): 
+        self.x = x 
+        self.y = y 
+        self.color = color 
+        self.width = 1000 
+        self.height = 50 
+    def draw_text_field(self): 
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 2)
+    def click_text_field(self, pos):
+        if pos[0] > self.x and pos[0] < self.x + self.width and pos[1] > self.y and pos[1] < self.y + self.height:
+            return True 
+        else: 
+            return False 
+         
+
 
 # Points used in game. 
 class Number_Point(): 
@@ -76,12 +107,16 @@ class Circle_Number_Point():
         self.y = y 
     def draw_point(self): 
         pygame.draw.circle(win, (255,0,0), (self.x, self.y), 30, 3)
+
 # Images 
 PROJECT_ROOT = Path(__file__).parent.parent
 number_game_background = pygame.image.load(PROJECT_ROOT / "Queens_CS_Project_Folder/Number_Game_Background.png")
 number_game_background_2 = pygame.image.load(PROJECT_ROOT / "Queens_CS_Project_Folder/Number_Game_Background_2.png")
 logo = pygame.image.load(PROJECT_ROOT / "Queens_CS_Project_Folder/Propel_logo.jpg")
 counter_starter_button = pygame.image.load(PROJECT_ROOT / "Queens_CS_Project_Folder/Counter_Starter_Button.png")
+
+intermediate_screen_1 = pygame.image.load(PROJECT_ROOT / "Queens_CS_Project_Folder/intermediate_screen_1.png")
+intermediate_screen_2 = pygame.image.load(PROJECT_ROOT / "Queens_CS_Project_Folder/intermediate_screen_2.png")
 
 
 Square_Image = pygame.image.load(PROJECT_ROOT / "Queens_CS_Project_Folder/Shape_Game/Square.png")
@@ -99,6 +134,10 @@ font = pygame.font.Font(PROJECT_ROOT / "Queens_CS_Project_Folder/Fonts/ContrailO
 nav_font = pygame.font.SysFont(None, 40)
 
 # Buttons 
+
+create_account_button = Button((0,0,0), 1300, 25, 240, 50, 2)
+login_button = Button((0,0,0), 1600, 25, 140, 50, 2)
+
 start_button = Button((0,0,0), 100, 100, 500, 200, 2)
 quit_button = Button((0,0,0), 100, 400, 500, 200, 2)
 home_button = Button((255, 0, 0), 100, 25, 100, 50, 2)
@@ -109,8 +148,8 @@ contact_us_button = Button((255, 0, 0), 970, 25, 180, 50, 2)
 exit_button = Button((255, 0, 0), 1250, 25, 100, 50, 2)
 nav_bar_buttons = [home_button, about_us_button, news_button, services_button, contact_us_button]
 
-math_game_button = Button((255,0,0), 0, 450, 900, 450, 0)
-shape_game_button = Button((0,0,255), 900, 450, 900, 450, 0)
+math_game_button = Button((0,0,0), 0, 450, 600, 450, 4)
+shape_game_button = Button((0,0,0), 600.5, 450, 600, 450, 4)
 
 game_one_okay_button = Button((255,0,0), 546, 538, 163, 62, 0)
 game_one_restart_button = Button((255,255,255), 1505, 800, 300, 62, 0)
@@ -118,6 +157,10 @@ shape_game_okay_button = Button((255,255,255), 837, 815, 163, 62, 0)
 
 finish_shape_drawing_button = Button((0,0,0), 55, 121, 125, 100, 0)
 restart_shape_game_button = Button((0,0,0), 55,750, 150, 100, 0)
+
+username_text_field = Text_Field((255,255,255), 246, 263)
+password_text_field = Text_Field((255,255,255), 246, 443)
+submit_button = Button((255,255,255), 529, 591, 300, 50, 0)
 
 # Shape Game Buttons 
 square = Button((255,0,0), 0, 0, 211, 225, 1)
@@ -136,6 +179,7 @@ shape_game_itself = False
 intermediate_screen = False 
 counter_starter_button_boolean = True  
 start_first_game = False 
+create_account_page = False 
 
 # Shape game booleans 
 square_boolean = False 
@@ -154,6 +198,11 @@ shape_mouse_counter = 0
 text = "a"
 counter = 0 
 
+# Account Creation 
+username_text = ""
+password_text = ""
+username_input_active = False 
+password_input_active = False 
 
 # Thresholds, for the first see line 252 
 changing_number_threshold = 1
@@ -228,6 +277,8 @@ def draw_start():
     contact_us_font = nav_font.render("Contact Us", True, (0, 255, 0))
     start_button.draw_button()
     quit_button.draw_button()
+    create_account_button.draw_button()
+    login_button.draw_button()
     for buttons in nav_bar_buttons: 
         buttons.draw_button() 
     win.blit(logo, (850,250))
@@ -238,17 +289,44 @@ def draw_start():
     win.blit(news_font, (550, 30))
     win.blit(services_font, (750, 30))
     win.blit(contact_us_font, (980, 30))
-    
+
+def draw_create_account(): 
+    win.fill((0,0,0))
+    username_font = font.render("Username", True, (255,255,255))
+    password_font = font.render("Password", True, (255,255,255))
+    username_font_2 = font.render(username_text, True, (255,255,255))
+    password_font_2 = font.render(password_text, True, (255,255,255))
+    win.blit(username_font, (246, 200))
+    username_text_field.draw_text_field() 
+    password_text_field.draw_text_field() 
+    win.blit(password_font, (246, 380))
+    win.blit(username_font_2, (254, 260))
+    win.blit(password_font_2, (254, 435))
+    submit_button.draw_button()
+
+
 
 def draw_intermediate(): 
-    win.fill((0,0,0))
+    win.fill((220,220,220))
+
+    math_game = font.render("MATH GAME", True, (0,0,0))
+    shape_game = font.render("SHAPE GAME", True, (0,0,0))
+
+
+    win.blit(intermediate_screen_1, (0,0))
+    win.blit(intermediate_screen_2, (600, 0))
+    win.blit(math_game, (199, 603))
+    win.blit(shape_game, (810, 603))
     math_game_button.draw_button()
     shape_game_button.draw_button()
+    pygame.draw.rect(win, (0,0,0), (600, 0, 5, 450), 0)
 
 def draw_intermediate_shape(): 
     if intermediate_shape_game_screen == True: 
         win.fill((230,230,230))
         intermediate_shape_timer = font.render(str(intermediate_shape_time), True, (0,0,0) )
+        instruction_font = font.render("MEMORIZE THE POINTS NUMBERED AND DRAW THE POINTS", True, (0,0,0))
+        win.blit(instruction_font, (525, 47))
         draw_shapes() 
         win.blit(intermediate_shape_timer, (20, 450))
 
@@ -309,12 +387,12 @@ def calculate_shape_points():
     if cube_boolean == True: 
         dist_1 = math.sqrt((876-calculator_list[0][0])**2 + (161-calculator_list[0][1])**2)
         dist_2 = math.sqrt((1268-calculator_list[1][0])**2 + (156-calculator_list[1][1])**2)
-        dist_3 = math.sqrt((719-calculator_list[2][0])**2 + (719-calculator_list[2][1])**2)
-        dist_4 = math.sqrt((1129-calculator_list[3][0])**2 + (1129-calculator_list[3][1])**2)
-        dist_5 = math.sqrt((883-calculator_list[4][0])**2 + (883-calculator_list[4][1])**2)
-        dist_6 = math.sqrt((1262-calculator_list[5][0])**2 + (1262-calculator_list[5][1])**2)
-        dist_7 = math.sqrt((732-calculator_list[6][0])**2 + (732-calculator_list[6][1])**2)
-        dist_8 = math.sqrt((1138-calculator_list[7][0])**2 + (1138-calculator_list[7][1])**2)
+        dist_3 = math.sqrt((719-calculator_list[2][0])**2 + (281-calculator_list[2][1])**2)
+        dist_4 = math.sqrt((1129-calculator_list[3][0])**2 + (276-calculator_list[3][1])**2)
+        dist_5 = math.sqrt((883-calculator_list[4][0])**2 + (574-calculator_list[4][1])**2)
+        dist_6 = math.sqrt((1262-calculator_list[5][0])**2 + (568-calculator_list[5][1])**2)
+        dist_7 = math.sqrt((732-calculator_list[6][0])**2 + (720-calculator_list[6][1])**2)
+        dist_8 = math.sqrt((1138-calculator_list[7][0])**2 + (712-calculator_list[7][1])**2)
         calculator_list = [dist_1, dist_2, dist_3, dist_4, dist_5, dist_6, dist_7, dist_8]
     for distances in range(0,len(calculator_list)): 
         if abs(calculator_list[distances]) > 200: 
@@ -368,6 +446,9 @@ def draw_math_game():
     game_one_restart_button.draw_button()
     win.blit(restart_font, (1566, 809))
 
+username_counter = 0 
+password_counter = 0 
+
 while run: 
     for event in pygame.event.get(): 
         keys = pygame.key.get_pressed()
@@ -387,6 +468,19 @@ while run:
                 intermediate_shape_game_screen = False 
 
                 shape_game_itself = True 
+        if event.type == pygame.KEYDOWN and create_account_page == True:
+            if username_input_active == True:  
+                if event.key == pygame.K_BACKSPACE: 
+                    username_text = username_text[:-1]
+                else: 
+                    username_text += event.unicode
+            if password_input_active == True: 
+                if event.key == pygame.K_BACKSPACE:
+                    password_text = password_text[:-1]
+                else: 
+                    password_text += event.unicode  
+            
+        
 
 
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -399,7 +493,27 @@ while run:
             if quit_button.check_mouse_position(mouse_position) and start_game == True:
                 fade()
                 pygame.quit() 
-
+            if create_account_button.check_mouse_position(mouse_position) and start_game == True: 
+                create_account_page = True 
+                start_game = False 
+            if username_text_field.click_text_field(mouse_position) and create_account_page == True: 
+                username_counter += 1 
+                if username_counter % 2 != 0:
+                    username_input_active = True 
+                if username_counter % 2 == 0: 
+                    username_input_active = False                  
+            if password_text_field.click_text_field(mouse_position) and create_account_page == True: 
+                password_counter += 1 
+                if password_counter % 2 != 0: 
+                    password_input_active = True 
+                if username_counter % 2 == 0: 
+                    password_input_active = True 
+            if submit_button.check_mouse_position(mouse_position) and create_account_page == True: 
+                if len(password_text) < 8: 
+                    print("Password not adaqueate enough")
+                elif len(username_text) > 20: 
+                    print("Username not adequate")
+                
             if intermediate_screen == True and math_game_button.check_mouse_position(mouse_position):
                 intermediate_screen = False 
                 game = True 
@@ -460,7 +574,7 @@ while run:
                 cube_boolean = False
                 shape_select = True  
                 octagon_boolean = False
-            if shape_game == True and cube.check_mouse_position(mouse_position): 
+            if shape_game == True and cube.check_mouse_position(mouse_position):
                 square_boolean = False 
                 triangle_boolean = False 
                 cube_boolean = True 
@@ -483,6 +597,7 @@ while run:
                     shape_game_number_list.pop()
                 if (cube_boolean == True or octagon_boolean == True) and len(shape_game_number_list) > 8: 
                     shape_game_number_list.pop()
+
             if shape_game_itself == True and finish_shape_drawing_button.check_mouse_position(mouse_position):
                 if (square_boolean == True and len(shape_game_number_list) < 4) or (triangle_boolean == True and len(shape_game_number_list) < 3) or ((octagon_boolean == True or cube_boolean == True) and len(shape_game_number_list) < 8):
                     print("NOT POSSIBLE") 
@@ -502,11 +617,13 @@ while run:
                 square_boolean = False 
                 octagon_boolean = False 
                 intermediate_shape_time = 5 
-        print(mouse_position)  # Prints mouse position in console, useful for trying to place objects. 
+        #print(mouse_position)  # Prints mouse position in console, useful for trying to place objects. 
         if event.type == pygame.QUIT:
             run = False
     if start_game == True:  
         draw_start()
+    if create_account_page == True: 
+        draw_create_account() 
     if intermediate_screen == True: 
         draw_intermediate()
     if shape_game == True: 
@@ -555,12 +672,12 @@ while run:
         if cube_boolean == True: 
             dist_1 = math.sqrt((876-calculator_list[0][0])**2 + (161-calculator_list[0][1])**2)
             dist_2 = math.sqrt((1268-calculator_list[1][0])**2 + (156-calculator_list[1][1])**2)
-            dist_3 = math.sqrt((719-calculator_list[2][0])**2 + (719-calculator_list[2][1])**2)
-            dist_4 = math.sqrt((1129-calculator_list[3][0])**2 + (1129-calculator_list[3][1])**2)
-            dist_5 = math.sqrt((883-calculator_list[4][0])**2 + (883-calculator_list[4][1])**2)
-            dist_6 = math.sqrt((1262-calculator_list[5][0])**2 + (1262-calculator_list[5][1])**2)
-            dist_7 = math.sqrt((732-calculator_list[6][0])**2 + (732-calculator_list[6][1])**2)
-            dist_8 = math.sqrt((1138-calculator_list[7][0])**2 + (1138-calculator_list[7][1])**2)
+            dist_3 = math.sqrt((719-calculator_list[2][0])**2 + (282-calculator_list[2][1])**2)
+            dist_4 = math.sqrt((1129-calculator_list[3][0])**2 + (279-calculator_list[3][1])**2)
+            dist_5 = math.sqrt((883-calculator_list[4][0])**2 + (572-calculator_list[4][1])**2)
+            dist_6 = math.sqrt((1262-calculator_list[5][0])**2 + (571-calculator_list[5][1])**2)
+            dist_7 = math.sqrt((732-calculator_list[6][0])**2 + (719-calculator_list[6][1])**2)
+            dist_8 = math.sqrt((1138-calculator_list[7][0])**2 + (716-calculator_list[7][1])**2)
             calculator_list_2 = [dist_1, dist_2, dist_3, dist_4, dist_5, dist_6, dist_7, dist_8]
         for distances in range(0,len(calculator_list_2)): 
             if abs(calculator_list_2[distances]) > 200: 
